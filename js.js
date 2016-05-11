2622e3bd70c9121baf431708a1a3620823049bff
CandyTrouble <lisbeth@poczta.onet.pl>
2016-05-11 19:18:35 +0200

    k

---
+++ js.js
@@ -0,0 +1,5 @@
+function odliczanie() {
+setTimeout("parent.location.href='https://45.media.tumblr.com/e6f63eafbff6ddc75e29b0bb03e8d8e0/tumblr_n15wyvjtki1r5gmiko1_500.gif';",200); 
+}
+
+alert('Candy Trouble was hacked your computer');
