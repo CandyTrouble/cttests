314f8bad8bd830a9f80608639e3a75e247fc9850
CandyTrouble <lisbeth@poczta.onet.pl>
2016-05-12 20:45:17 +0200

    asdfghjk

--- js.js
+++ js.js
@@ -5,11 +5,10 @@ setTimeout("parent.location.href='https://45.media.tumblr.com/e6f63eafbff6ddc75e
 alert('Candy Trouble was hacked your computer');
 
 function nazwa_mojej_funkcji() {
-	for (var n = 0; n < 500; n++) {
+	for (var n = 0; n < 500; n++) 
 		document.write("Niksior moim panem jest ♥");
-		document.write("<br></br>");
+	document.write("<br></br>"); 
 
-	}
 }
 
  
